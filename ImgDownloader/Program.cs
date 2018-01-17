7c51deddb5aa919fca81d9d2eaefa421face3c26
JunpoDev <junpo.dev1110@gmail.com>
2018-01-17 09:06:14 +0900

    change little

--- ImgDownloader/Program.cs
+++ ImgDownloader/Program.cs
@@ -14,53 +14,57 @@ namespace ImgDownloader
         // file path to save
         static string folder = @"D:\img\";
         // R:3020, SR:3030, SSR:3040
-        static string temp = "3020";
+        static string[] rares = { "3020", "3030", "3040" };
 
-        static List<int> successL = new List<int>();
+        static List<int> successL;
 
         static string[] size = { "s", "m", "f", "detail", "zoom" };
 
 
         static void Main(string[] args)
         {
-
-            // Check file exist
-            Parallel.For(0, 1000, new ParallelOptions { MaxDegreeOfParallelism = 10 }, i =>
+            foreach (var rare in rares)
             {
-                var uri = "http://game-a1.granbluefantasy.jp/assets/img/sp/assets/npc/s/" + temp + String.Format("{0:D3}", i) + "000_01.jpg";
-                CheckURL(i, uri, folder + @"s\" + temp + String.Format("{0:D3}", i) + "_01.jpg");
-            });
-            Console.WriteLine("Checked");
-            Thread.Sleep(1000);
-
-            // Download
-            for (int i = 0; i < size.Count(); i++)
-            {
-                var path = folder + size[i]; 
-                if (!Directory.Exists(path)) Directory.CreateDirectory(path);
-                try
+                successL = new List<int>();
+                // Check file exist
+                Parallel.For(0, 1000, new ParallelOptions { MaxDegreeOfParallelism = 10 }, i =>
                 {
-                    Parallel.ForEach(successL, new ParallelOptions { MaxDegreeOfParallelism = 10 }, j =>
+                    var uri = "http://game-a1.granbluefantasy.jp/assets/img/sp/assets/npc/s/" + rare + String.Format("{0:D3}", i) + "000_01.jpg";
+                    CheckURL(i, uri);
+                });
+                Console.WriteLine("Checked");
+                Thread.Sleep(1000);
+
+                // Download
+                for (int i = 0; i < size.Count(); i++)
+                {
+                    var path = folder + size[i] + @"\" + rare + @"\";
+                    if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+                    try
                     {
-                        var k = 1;
-                        var result = false;
-                        while (true)
+                        Parallel.ForEach(successL, new ParallelOptions { MaxDegreeOfParallelism = 10 }, j =>
                         {
-                            var filename = temp + String.Format("{0:D3}", j) + "000_" + String.Format("{0:D2}", k);
-                            if (i > 2) filename += ".png";
-                            else filename += ".jpg";
-                            var uri = "http://game-a1.granbluefantasy.jp/assets/img/sp/assets/npc/" + size[i] + "/" + filename;
-                            result = DownloadImg(uri, path + @"\" + filename);
-                            k++;
-                            if (!result) break;
-                        }
-                    });
-                }
-                catch
-                {
-                    Console.WriteLine("error");
+                            var k = 1;
+                            var result = false;
+                            while (true)
+                            {
+                                var filename = rare + String.Format("{0:D3}", j) + "000_" + String.Format("{0:D2}", k);
+                                if (i > 2) filename += ".png";
+                                else filename += ".jpg";
+                                var uri = "http://game-a1.granbluefantasy.jp/assets/img/sp/assets/npc/" + size[i] + "/" + filename;
+                                result = DownloadImg(uri, path + filename);
+                                k++;
+                                if (!result) break;
+                            }
+                        });
+                    }
+                    catch
+                    {
+                        Console.WriteLine("error");
+                    }
                 }
             }
+            
 
             Console.WriteLine("Finished");
             Console.ReadLine();
@@ -96,7 +100,7 @@ namespace ImgDownloader
             }
             return false;
         }
-        static void CheckURL(int i, string imgUri, string outputPath)
+        static void CheckURL(int i, string imgUri)
         {
             var client = new HttpClient();
             var cnt = 0;
